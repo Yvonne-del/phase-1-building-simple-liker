8007bbb64e966df9e5a45635050eeea2b7aaa6e1
Yvonne-del <ys359375@gmail.com>
2025-04-02 17:30:50 +0300

    creates a like button

--- main.js
+++ main.js
@@ -3,9 +3,33 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
-
+document.addEventListener("DOMContentLoaded", () => {
+  const likeGlyphs = document.querySelectorAll(".like-glyph");
+  const errorModal = document.getElementById("modal");
+  const modalMessage = document.getElementById("modal-message");
+  
+  likeGlyphs.forEach(glyph => {
+    glyph.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          if (glyph.textContent === EMPTY_HEART) {
+            glyph.textContent = FULL_HEART;
+            glyph.classList.add("activated-heart");
+          } else {
+            glyph.textContent = EMPTY_HEART;
+            glyph.classList.remove("activated-heart");
+          }
+        })
+        .catch(error => {
+          modalMessage.textContent = error;
+          errorModal.classList.remove("hidden");
+          setTimeout(() => {
+            errorModal.classList.add("hidden");
+          }, 3000);
+        });
+    });
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
